6c024b9aae91abc4360146e6574457730c0f05a1
GrecuThomas <grecu.thomas@yahoo.com>
2026-03-19 10:32:00 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 import CakeTierDesigner from '../components/CakeTierDesigner'
+import Main_Page from '../components/Main_Page'
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
-  return <CakeTierDesigner />
+  return <Main_Page />
 }
